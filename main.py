33fc07be32d987390f68be431661793492d9a8ce
jacobz27182 <jzhang27182@gmail.com>
2026-04-10 20:45:21 +1000

    python code update

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import serial
-import sys
+import serial.tools.list_ports
 import time
-
+import keyboard
 
 def main():
-    port = sys.argv[1] if len(sys.argv) > 1 else "COM8"
+    port = serial.tools.list_ports.comports()[0]
     baudrate = 115200
 
     try:
         with serial.Serial(port=port, baudrate=baudrate, timeout=0.5) as ser:
             print(f"Connected to {port} at {baudrate} baud")
-
-            #msg = str(6)
-            ser.write(b'B')
-            print("Listening for STM32 output. Press Ctrl+C to stop.")
+            
+            msg = input("Enter message to send: ").encode()
+ 
+            # print("Listening for STM32 output. Press Ctrl+C to stop.")
 
             while True:
+                ser.write(str(len(msg)).encode())
+                ser.read() #reads 1 byte if no arguments fed
+                # print("Acklowledgement Receiveds.")
+                ser.write(msg)
+
                 line = ser.readline()
                 if not line:
                     continue
-                try:
-                    #print(line)
-                    #print(line.decode("utf-8"))
-                    print(line.decode())
-                except Exception:
-                    print(repr(line))
-                time.sleep(0.01)
+
+                print(line.decode())
+                
+                keyboard.wait()
 
     except serial.SerialException as exc:
         print("Serial error:", exc)
