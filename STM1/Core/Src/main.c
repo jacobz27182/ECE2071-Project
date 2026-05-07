9c0c07a985caf1552ded1505a94614b41d05209b
jacobz27182 <jzhang27182@gmail.com>
2026-05-07 18:20:20 +1000

    set SPI

--- STM1/Core/Src/main.c
+++ STM1/Core/Src/main.c
@@ -280,7 +280,7 @@ static void MX_SPI1_Init(void)
   /* SPI1 parameter configuration*/
   SPI_InitStruct.TransferDirection = LL_SPI_FULL_DUPLEX;
   SPI_InitStruct.Mode = LL_SPI_MODE_MASTER;
-  SPI_InitStruct.DataWidth = LL_SPI_DATAWIDTH_10BIT;
+  SPI_InitStruct.DataWidth = LL_SPI_DATAWIDTH_12BIT;
   SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_LOW;
   SPI_InitStruct.ClockPhase = LL_SPI_PHASE_1EDGE;
   SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
@@ -421,7 +421,10 @@ static void SPI1_Write2Bytes(uint16_t tx_bytes){
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc){
 	  uint16_t sampledValue = (uint16_t)HAL_ADC_GetValue(&hadc1);
-	  SPI1_Write2Bytes(sampledValue);
+	  char buffer[100];
+	  sprintf(buffer,"%d\r\n",sampledValue);
+	  HAL_UART_Transmit(&huart2,buffer,8,HAL_MAX_DELAY);
+//	  SPI1_Write2Bytes(sampledValue);
 }
 /* USER CODE END 4 */
 
