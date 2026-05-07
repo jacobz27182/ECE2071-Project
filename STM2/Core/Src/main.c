ca3e1e8639f7a93926eb6385e96d252239b74531
jacobz27182 <jzhang27182@gmail.com>
2026-05-07 21:04:47 +1000

    small fix

--- STM2/Core/Src/main.c
+++ STM2/Core/Src/main.c
@@ -33,8 +33,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define N 4 // number of samples to average
-#define THRESHOLD 60 // threshold for the average value
+#define N 1 // number of samples to average
+#define THRESHOLD 9999 // threshold for the average value
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -226,7 +226,7 @@ int main(void)
 			 index = (index + 1) % N; // so the index will cycle through the buffer from 0 to N-1
 
 				HAL_GPIO_WritePin(Debug2_GPIO_Port,Debug2_Pin,1);
-			uint8_t sample8 = mean >> 4; //shift the mean by 2 bits so that from 10 bit to 8 bit
+			uint8_t sample8 = mean; //shift the mean by 2 bits so that from 10 bit to 8 bit
 			uint8_t sample8pt2 = mean >> 8;
 			while (!(huart2.Instance->ISR & USART_ISR_TXE));
 			huart2.Instance->TDR = sample8;
