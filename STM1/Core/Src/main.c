bc0d650a5903293c6e9481c5a4faaa2bd5cc2608
jacobz27182 <jzhang27182@gmail.com>
2026-05-09 00:06:49 +1000

    fine tune prescaler

--- STM1/Core/Src/main.c
+++ STM1/Core/Src/main.c
@@ -318,7 +318,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 5;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 117;
+  htim1.Init.Period = 120;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
